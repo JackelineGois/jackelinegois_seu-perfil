75abd8a18c0342f8d740e9d133fd74b66eb2213c
JackelineGois <jackelinedemoraisgois@gmail.com>
2023-04-29 12:31:34 +0100

    5- Ajustando o CSS do ProfileCard Component

--- src/components/profile-card/index.js
+++ src/components/profile-card/index.js
@@ -2,7 +2,7 @@ import { HStack, Avatar, Box, Text } from "native-base";
 
 export const ProfileCard = () => {
   return (
-    <HStack>
+    <HStack width={"100%"}>
       <Avatar mr={2} />
       <Box>
         <Text> Name</Text>
